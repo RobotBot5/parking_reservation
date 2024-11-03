a2f987d5cd35fce1ce37222bd578e3d02ccf8351
RobotBot <robotbotyourube@gmail.com>
2024-11-03 18:09:59 +0300

    Initial commit

---
+++ src/main/java/ru/robotbot/parking_reservation/domain/entities/UserEntity.java
@@ -0,0 +1,30 @@
+package ru.robotbot.parking_reservation.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String roles;
+
+    private String email;
+
+}
---
+++ src/test/java/ru/robotbot/parking_reservation/ParkingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.robotbot.parking_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
